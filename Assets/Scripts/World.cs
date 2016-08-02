5e5ee75f065cd7089b50bd6ce67834acfaad91ba
Michael Gøtzhe <migo@tv2.dk>
2016-08-02 14:26:03 +0200

    added more

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,9 +4,6 @@ using System.Collections;
 public class World : MonoBehaviour {
 	//http://maps.googleapis.com/maps/api/geocode/json?latlng=55.4018502,10.3419367&zoom=18&size=512x512&scale=2&maptype=roadmap&format=xml
 
-	public float titleOffSetTop = -0.0027567f;
-	public float titleOffSetLeft = 0.001498f;
-
 	public double latitude = 55.4018502;
 	public double longtitude = 10.3419367;
 
